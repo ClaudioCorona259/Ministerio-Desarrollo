7af4180db3dbd0bb99cbc29850517dc4858eee15
ClaudioCorona259 <claudioignacio259@gmail.com>
2024-10-15 23:45:10 -0300

    Cambio API

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ let cooperativaData = [];
 
 async function fetchCoop() {
     try {
-        const response = await fetch('https://apicoopmut20241015005242.azurewebsites.net/');
+        const response = await fetch('https://apicoopmut20241015005242.azurewebsites.net/api/Coop');
         cooperativaData = await response.json(); 
     } catch (error) {
         console.error('Error al cargar datos:', error);
